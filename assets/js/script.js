e1868b66b6fcf444d145afe54090d9e884771bd8
asouil <amaella.souil@gmail.com>
2019-05-07 12:27:27 +0200

    js-2-1

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,29 @@ for(var i=0; i<nblignes;i++){
 	plateau[i]=[];
 }
 
+NewGame();
+
+function NewGame(){
+	for(var i=0; i<nblignes;i++){
+		for(var j=0;j<nbcolonnes;j++){
+			plateau[i][j]=0;
+		}
+	}
+	var joueur=1;
+	afficheTextAnnonce("Le jeu commence, c'est au tour du joueur"+nomDuJoueur(joueur));
+	jeu=true;
+	creerTableau();
+}
+
+function afficheTextAnnonce(texte){
+	document.getElementById("text-annonce").innerHTML=texte;
+}
+
+function nomDuJoueur(numjoueur){
+	if(numjoueur==1){
+		return "rouge";
+	}else{
+		return "bleu";
+	}
+}
+
