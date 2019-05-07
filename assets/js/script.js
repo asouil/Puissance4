d7790f9bfd3ac3eb92654a38f9852578595a08c3
asouil <amaella.souil@gmail.com>
2019-05-07 14:05:43 +0200

    js-2-2

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ function NewGame(){
 		}
 	}
 	var joueur=1;
-	afficheTextAnnonce("Le jeu commence, c'est au tour du joueur"+nomDuJoueur(joueur));
+	afficheTextAnnonce("Le jeu commence, c'est au tour du joueur "+nomDuJoueur(joueur));
 	jeu=true;
 	creerTableau();
 }
@@ -33,5 +33,24 @@ function nomDuJoueur(numjoueur){
 	}else{
 		return "bleu";
 	}
+}
+
+function creerTableau(){
+	txt+="<table>";
+	for(var i=0;i<nblignes;i++){
+		txt+="<tr>";
+		for(var j=0;j<nbcolonnes;j++){
+			txt+="<td onclick='detecteclic(j)' id='index"+i+"-index"+j+"'>";
+			if(plateau[i][j]==1){
+				txt+="<div class='joueur1'></div>";
+			}else if(plateau[i][j]==2){
+				txt+="<div class='joueur2'></div>";
+			}
+			txt+="</td>";
+		}
+		txt+="</tr>";
+	}
+	txt+="</table>";
+	document.getElementById(puissance4).innerHTML=texte;
 }
 
